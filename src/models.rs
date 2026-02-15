71f404460794660af1a49966ab568d8e8c8efe77
DennisKlpng <dennis.kloepping@web.de>
2026-02-15 22:24:51 +0100

    Changed categories to personal needs

--- src/models.rs
+++ src/models.rs
@@ -4,25 +4,46 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 #[serde(rename_all = "lowercase")]
 pub enum TypeCategory {
-    A,
-    B,
-    C,
+    T1,
+    T2,
+    T3,
+    T4,
+    T5,
+    T6,
+    T7,
+    T8,
+    T9,
+    T10
 }
 
 impl TypeCategory {
     pub fn as_str(&self) -> &str {
         match self {
-            TypeCategory::A => "A",
-            TypeCategory::B => "B",
-            TypeCategory::C => "C",
+            TypeCategory::T1 => "Lebensmittel&Haushaltsbedarf",
+            TypeCategory::T2 => "Freizeit",
+            TypeCategory::T3 => "Mobilität",
+            TypeCategory::T4 => "Kleidung",
+            TypeCategory::T5 => "Elektronik",
+            TypeCategory::T6 => "Wohnungseinrichtung",
+            TypeCategory::T7 => "Urlaub",
+            TypeCategory::T8 => "Miete",
+            TypeCategory::T9 => "Versicherungen",
+            TypeCategory::T10 => "Sonstiges",
         }
     }
 
     pub fn from_str(s: &str) -> Option<Self> {
         match s {
-            "A" => Some(TypeCategory::A),
-            "B" => Some(TypeCategory::B),
-            "C" => Some(TypeCategory::C),
+            "Lebensmittel&Haushaltsbedarf" => Some(TypeCategory::T1),
+            "Freizeit" => Some(TypeCategory::T2),
+            "Mobilität" => Some(TypeCategory::T3),
+            "Kleidung" => Some(TypeCategory::T4),
+            "Elektronik" => Some(TypeCategory::T5),
+            "Wohnungseinrichtung" => Some(TypeCategory::T6),
+            "Urlaub" => Some(TypeCategory::T7),
+            "MieteC" => Some(TypeCategory::T8),
+            "Versicherungen" => Some(TypeCategory::T9),
+            "Sonstiges" => Some(TypeCategory::T10),
             _ => None,
         }
     }
@@ -31,25 +52,25 @@ impl TypeCategory {
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 #[serde(rename_all = "lowercase")]
 pub enum NecessityCategory {
-    D,
-    E,
-    F,
+    N1,
+    N2,
+    N3
 }
 
 impl NecessityCategory {
     pub fn as_str(&self) -> &str {
         match self {
-            NecessityCategory::D => "D",
-            NecessityCategory::E => "E",
-            NecessityCategory::F => "F",
+            NecessityCategory::N1 => "Notwendig",
+            NecessityCategory::N2 => "Nützlich",
+            NecessityCategory::N3 => "Luxus",
         }
     }
 
     pub fn from_str(s: &str) -> Option<Self> {
         match s {
-            "D" => Some(NecessityCategory::D),
-            "E" => Some(NecessityCategory::E),
-            "F" => Some(NecessityCategory::F),
+            "Notwendig" => Some(NecessityCategory::N1),
+            "Nützlich" => Some(NecessityCategory::N2),
+            "Luxus" => Some(NecessityCategory::N3),
             _ => None,
         }
     }
